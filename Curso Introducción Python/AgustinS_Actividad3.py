4416f77a0dfbe3f3bdb2bb968bbfa722fca8c795
agtoez <agtoez@gmail.com>
2025-02-22 03:47:11 -0300

    Actividad 3 A

---
+++ Curso Introducción Python/AgustinS_Actividad3.py
@@ -0,0 +1,27 @@
+import random
+
+def adivinarNumero():
+    numeroSecreto = random.randint(1, 10)
+    intentos = 0
+
+    while True:
+        try:
+            intento = int(input("Adivina el número (entre 1 y 10): "))
+            intentos += 1
+
+            if intento < 1 or intento > 10:
+                print("Por favor, ingresa un número entre 1 y 10.")
+                continue
+
+            if intento < numeroSecreto:
+                print("El número es mayor")
+            elif intento > numeroSecreto:
+                print("El número es menor")
+            else:
+                print(f"¡Felicidades! El número es: {numeroSecreto} y fue adivinado en {intentos} intentos.")
+        
+        except ValueError:
+            print('Entrada no válida. Ingresa un número entero.')
+
+
+adivinarNumero()
