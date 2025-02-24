ba3e2f5350ef7c17680855515d5d62ba6475b698
agtoez <agtoez@gmail.com>
2025-02-24 06:32:59 -0300

    Actividad 4 empezada

---
+++ Curso Introducción Python/AgustinS_Actividad4.py
@@ -0,0 +1,32 @@
+def menu():
+    print("\n📙 MENÚ DE CONTACTOS")    
+    print("1. Agregar contacto")
+    print("2. Mostrar todos los contactos")    
+    print("3. Buscar contacto por nombre")
+    print("4. Eliminar contacto")
+    print("5. Actualizar contacto")
+    print("6. Salir")
+
+# def agregarContacto(agenda):
+#     nombre = input("Ingrese el nombre: ").strip()
+#     if any(contacto[0].lower() == nombre.lower() for contacto in agenda):
+#         print("❌Contacto existente")
+#         return
+    
+#     telefono = input("Ingrese el teléfono: ").strip()
+#     email = input("Ingrese el email: ").strip()
+#     agenda.append((nombre, telefono, email))
+#     print("✅Contacto agendado")
+
+def mostrarContacto(agenda):
+    pass
+def buscarContacto(agenda):
+    pass
+def eliminarContacto(agenda):
+    pass
+def actualizarContacto(agenda):
+    pass
+def main():
+    pass
+
+menu()
