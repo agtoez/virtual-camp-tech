7c0b1683d1911ca7215b7164c352e8fb81e77c04
agtoez <agtoez@gmail.com>
2025-02-26 18:11:42 -0300

    Actividad 1 Curso de Automatizaciones con Python.

--- Automatizaciones con Python/AgustinS_Práctica1.py
+++ Automatizaciones con Python/AgustinS_Práctica1.py
@@ -1,14 +1,18 @@
 from pathlib import Path
 import shutil
 
-CARPETA_ORIGEN = Path("Descargas")
+CARPETA_ORIGEN = Path(input("Seleccione el directorio: ")).expanduser()
 
 CATEGORIAS = {
     "Documentos en PDF": [".pdf"],
     "Imágenes": [".jpg", ".jpeg", ".png", ".gif"],
     "Música": [".mp3", ".wav"],
     "Archivos ZIP": [".zip", ".rar" ],
-    "Python": [".py"]
+    "Python": [".py"],
+    "Ejecutables": [".exe", ".msi", ".bat", ".sh"],
+    "Documentos": [".doc", ".docx", ".txt"],
+    "Otro": []
+
 }
 
 def clasificarArchivos():
@@ -16,17 +20,27 @@ def clasificarArchivos():
         print(f"La carpeta '{CARPETA_ORIGEN}' no existe.")
         return
     
+    archivosMovidos = {categoria: 0 for categoria in CATEGORIAS}
+    archivosMovidos["Otros"] = 0
+    
     for archivo in  CARPETA_ORIGEN.iterdir():
         if archivo.is_file():
             extension = archivo.suffix.lower()
 
+            categoriaDestino = "Otros"
             for categoria, extensiones in CATEGORIAS.items():
                 if extension in extensiones:
-                    carpetaDestino = CARPETA_ORIGEN / categoria
-                    carpetaDestino.mkdir(exist_ok=True)
-                    archivo.rename(carpetaDestino / archivo.name)
-                    print(f"Movido: {archivo.name} -> {categoria}")
+                    categoriaDestino = categoria
                     break
+
+            carpetaDestino = CARPETA_ORIGEN / categoria
+            carpetaDestino.mkdir(exist_ok=True)
+            archivo.rename(carpetaDestino / archivo.name)
+            archivosMovidos[categoriaDestino] += 1
+
+    for categoria, cantidad in archivosMovidos.items():
+        if cantidad > 0:
+            print(f"{cantidad} archivo(s) movido(s) a la carpeta {categoria}/")
 
 if __name__ == "__main__":
     clasificarArchivos()
